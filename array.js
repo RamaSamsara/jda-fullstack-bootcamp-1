90ed1682f23ba3d6faa248e99dddcb4312cca614
Rama Samsara <thegiantcomo2@gmail.com>
2025-05-27 06:46:07 +0700

    Add array.js to master branch

---
+++ array.js
@@ -0,0 +1,14 @@
+//array 
+//regular var declaration
+var day1 = Monday
+var day2 = Tuesday
+var day3 = Wednesday
+var day4 = Thursday
+var day5 = Friday
+var day6 = Saturday
+var day7 = Sunday
+
+//with var array concept
+
+var day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
