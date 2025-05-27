26ff47b619efdef1c9cc5b546edc352c246d291b
Rama Samsara <thegiantcomo2@gmail.com>
2025-05-28 06:16:23 +0700

    Add array.js to master branch

--- array.js
+++ array.js
@@ -8,7 +8,19 @@ var day5 = Friday
 var day6 = Saturday
 var day7 = Sunday
 
-//with var array concept
+//with regular var array concept
 
 var day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+// Array Manipulation 
+// - Adding Array
+
+var day = []
+day[0] = "Monday"
+day[1] = "Tuesday"
+day[2] = "Wednesday"
+day[3] = "Thursday"
+day[4] = "Friday"
+day[5] = "Saturday"
+day[6] = "Sunday"
 
