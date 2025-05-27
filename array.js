c2504de4c577ff48d651c0e197cc373b5ea433fc
Rama Samsara <thegiantcomo2@gmail.com>
2025-05-28 06:48:15 +0700

    Add array.js to master branch

--- array.js
+++ array.js
@@ -1,4 +1,6 @@
 //array 
+
+
 //regular var declaration
 var day1 = Monday
 var day2 = Tuesday
@@ -9,12 +11,11 @@ var day6 = Saturday
 var day7 = Sunday
 
 //with regular var array concept
-
 var day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
 // Array Manipulation 
-// - Adding Array
 
+// - Adding Array
 var day = []
 day[0] = "Monday"
 day[1] = "Tuesday"
@@ -23,4 +24,15 @@ day[3] = "Thursday"
 day[4] = "Friday"
 day[5] = "Saturday"
 day[6] = "Sunday"
+
+// how to delete array on index 1 
+var day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+arr[1] = undefined;
+//when running the code by type console.log(day[1]) will result = undefined
+
+//showing array 
+var day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+for(var i = 0; i < day.length; i++ ) {
+    console.log(day);
+}
 
